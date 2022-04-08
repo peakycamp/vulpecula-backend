732c44e294b2565229ecc171a1440050f60f5076
Rafael de Macêdo <r4faelmf@gmail.com>
2022-04-07 21:50:55 -0300

    first test

--- vulpecula/core/tests.py
+++ vulpecula/core/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserTests(APITestCase):
+	def setUp(self):
+		User.objects.create_user('admin', 'admin@admin.com', 'vulpecula')
+
+	def test_get_user(self):
+		self.client.login(username='admin', password='vulpecula')
+		response = self.client.get('/users/', format='json')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.client.logout()
